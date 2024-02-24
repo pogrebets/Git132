3b0ba76c0647c211e091477862f153ec6ae32db1
Marusi1a <marusischasodiyka@gmail.com>
2024-02-24 21:55:23 +0200

    нсд

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ from utils.py import factorial
 number = int(input())
 result = factorial(number)
 print(fThe factorial of {number} is {result})
+
+from utils.py import find_gcd
+
+num1 = int(input("Введіть перше число: "))
+num2 = int(input("Введіть друге число: "))
+
+gcd = find_gcd(num1, num2)
+
+print(f"НСД({num1}, {num2}) = {gcd}
+
--- utils.py
+++ utils.py
@@ -4,3 +4,8 @@ def factorial(n):
     else:
         return n * factorial(n-1)
 
+def find_gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
